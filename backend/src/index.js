1b1133046c09b9d831dd78d71571281d49ebb8cb
egadestaviano <pastilaku121@gmail.com>
2025-12-02 14:50:13 +0700

    Integrate Socket.IO server with Express application

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 import "dotenv/config";
 import web from "./middleware/web.js";
+import { initializeSocket } from "./utils/socket.js";
+
 const PORT = process.env.PORT || 3000;
 
-web.listen(PORT, () => {
+const server = web.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
+
+initializeSocket(server);
